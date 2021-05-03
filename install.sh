039e73c89fab67bb034129f2121dc2a74e63d660
Fernando Guerrero <devrrior@gmail.com>
2021-05-02 23:03:42 -0500

    feat(add install.sh): add install.sh
    I made a file install.sh, that will work for install downtube
    
    BREAKING CHANGE: Install script

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 # Welcome
-echo "Cloning files"
-echo "Cloning repository"
-git clone https://github.com/devrrior/youtube-downloader-cli.git
-echo "Installing required modules"
+echo "Cloning files..."
+echo "Cloning repository..."
+git clone https://github.com/devrrior/youtube-downloader-cli/
+echo "Installing required modules..."
 pip install -r youtube-downloader-cli/requirements.txt
-echo "Copy script"
+echo "Copy script..."
 cp youtube-downloader-cli/downtube /usr/local/bin
-echo "Delte files"
+echo "Delete files..."
 rm -rf youtube-downloader-cli/
+echo "That's done!"
